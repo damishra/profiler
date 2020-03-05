3f3adc33bcbf20329047b827492ed72f786a4ab1
Dishant Mishra <dmishra@pop-os.localdomain>
2020-03-05 15:53:09 -0500

    feat: finished processes script - damishra

--- scripts/processes.sh
+++ scripts/processes.sh
@@ -1,12 +1,27 @@
 #! /usr/bin/bash
 
-bandwidth_hog_pid=$(pidof bandwidth_hog)
-bandwidth_hog_burst_pid=$(pidof bandwidth_hog_burst)
-cpu_hog_pid=$(pidof cpu_hog)
-disk_hog_pid=$(pidof disk_hog)
-memory_hog_pid=$(pidof memory_hog)
-memory_hog_leak_pid=$(pidof memory_hog_leak)
+process_array=("bandwidth_hog_burst" "bandwidth_hog" "cpu_hog" "disk_hog" "memory_hog" "memory_hog_leak")
 
+kill_all_processes() {
+    for process in "${process_array[@]}"; do
+        kill $(pidof "$process")
+    done
+    exit 0
+}
+
+trap kill_all_processes SIGINT SIGTERM
+
+for process in "${process_array[@]}"; do
+    ./../resources/bin/$process 100.65.64.174 &
+    echo "seconds,%cpu,%mem" >"$process".csv
+done
+
+counter=0
 while true; do
-
+    sleep 5
+    ((counter = counter + 5))
+    for process in "${process_array[@]}"; do
+        output=$(ps -p $(pidof "$process") -o %cpu,%mem | tail -n +2)
+        echo "$counter $output" | sed "s/\s\+/,/g" 1>>"$process".csv
+    done
 done
