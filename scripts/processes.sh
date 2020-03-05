dc42d2fa6123b7ed52f5ce0d2d0ef318a96c532a
Dishant Mishra <dmishra@pop-os.localdomain>
2020-03-05 16:15:51 -0500

    chore: moved process script to profiler.sh

--- scripts/processes.sh
+++ scripts/processes.sh
@@ -2,6 +2,7 @@
 
 process_array=("bandwidth_hog_burst" "bandwidth_hog" "cpu_hog" "disk_hog" "memory_hog" "memory_hog_leak")
 
+# trap process to kill all running processes started by the script.
 kill_all_processes() {
     for process in "${process_array[@]}"; do
         kill $(pidof "$process")
@@ -11,17 +12,22 @@ kill_all_processes() {
 
 trap kill_all_processes SIGINT SIGTERM
 
+get_process_metrics() {
+    for process in "${process_array[@]}"; do
+        output=$(ps -p $(pidof "$process") -o %cpu,%mem | tail -n +2)
+        echo "$counter $output" | sed "s/\s\+/,/g" 1>>"$process"_metrics.csv
+    done
+}
+
+# loop to start all processes required for monitoring.
 for process in "${process_array[@]}"; do
     ./../resources/bin/$process 100.65.64.174 &
-    echo "seconds,%cpu,%mem" >"$process".csv
+    echo "seconds,%cpu,%mem" >"$process"_metrics.csv
 done
 
 counter=0
 while true; do
     sleep 5
     ((counter = counter + 5))
-    for process in "${process_array[@]}"; do
-        output=$(ps -p $(pidof "$process") -o %cpu,%mem | tail -n +2)
-        echo "$counter $output" | sed "s/\s\+/,/g" 1>>"$process".csv
-    done
+    get_process_metrics
 done
