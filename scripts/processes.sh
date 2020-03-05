38c43acde61f3e050c636bdf8b9998592d322006
Dishant Mishra <dmishra@pop-os.localdomain>
2020-03-04 22:27:45 -0500

    feat: processes

---
+++ scripts/processes.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+bandwidth_hog_pid=$(pidof bandwidth_hog)
+bandwidth_hog_burst_pid=$(pidof bandwidth_hog_burst)
+cpu_hog_pid=$(pidof cpu_hog)
+disk_hog_pid=$(pidof disk_hog)
+memory_hog_pid=$(pidof memory_hog)
+memory_hog_leak_pid=$(pidof memory_hog_leak)
+
+while true; do
+
+done
--- scripts/resources.sh
+++ scripts/resources.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-get_idle_cpu () {
-    local output=$( iostat -c )
-    local line=$( echo "$output" | sed -n 4p | sed 's/  */ /g' )
+get_idle_cpu() {
+    local output=$(iostat -c)
+    local line=$(echo "$output" | sed -n 4p | sed 's/  */ /g')
 
-    local system=$( echo "$line" | cut -d " " -f 7 )
+    local system=$(echo "$line" | cut -d " " -f 7)
     echo "$system"
 }
 
-get_free_mem () {
-    local output=$( free | sed -n 2p | sed 's/  */ /g' )
-    local freemem=$( echo "$output" | cut -d " " -f 4 )
+get_free_mem() {
+    local output=$(free | sed -n 2p | sed 's/  */ /g')
+    local freemem=$(echo "$output" | cut -d " " -f 4)
     echo "$freemem"
 }
 
