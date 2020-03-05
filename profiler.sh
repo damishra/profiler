78b9854e3ef7cb117b373186807c8982bf5dc442
Dishant Mishra <dmishra@pop-os.localdomain>
2020-03-05 16:32:48 -0500

    feat: started work on system data

--- profiler.sh
+++ profiler.sh
@@ -1,1 +1,39 @@
+#! /usr/bin/bash
 
+process_array=("bandwidth_hog_burst" "bandwidth_hog" "cpu_hog" "disk_hog" "memory_hog" "memory_hog_leak")
+
+# trap process to kill all running processes started by the script.
+kill_all_processes() {
+    for process in "${process_array[@]}"; do
+        kill $(pidof "$process")
+    done
+    exit 0
+}
+
+trap kill_all_processes SIGINT SIGTERM
+
+# function to retrieve process metrics.
+get_process_metrics() {
+    for process in "${process_array[@]}"; do
+        local output=$(ps -p $(pidof "$process") -o %cpu,%mem | tail -n +2)
+        echo "$counter $output" | sed "s/\s\+/,/g" 1>>"$process"_metrics.csv
+    done
+}
+
+# function to retrieve system metrics.
+get_system_metrics() {
+
+}
+
+# loop to start all processes required for monitoring.
+for process in "${process_array[@]}"; do
+    ./../resources/bin/$process 100.65.64.174 &
+    echo "seconds,%cpu,%mem" >"$process"_metrics.csv
+done
+
+counter=0
+while true; do
+    sleep 5
+    ((counter = counter + 5))
+    get_process_metrics
+done
